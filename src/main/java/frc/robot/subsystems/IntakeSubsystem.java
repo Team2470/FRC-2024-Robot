6923fff69a492689a439186f56d5ac4aebc11e37
Soren Bennett <benns270@isd271.org>
2024-01-17 20:47:10 -0600

    Initial push for intake subsystem

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,56 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+public class IntakeSubsystem extends SubsystemBase {
+    private final CANSparkFlex m_intake; 
+    private final RelativeEncoder m_encoder;
+    public IntakeSubsystem(int canID) {
+        m_intake = new CANSparkFlex(canID, MotorType.kBrushless);
+
+        m_intake.restoreFactoryDefaults();
+        m_intake.setInverted(false);
+        m_intake.setSmartCurrentLimit(40);
+
+        m_encoder = m_intake.getEncoder();
+        m_intake.burnFlash();       
+
+    }
+    public double getVelocity(){
+        return m_encoder.getVelocity();
+  }
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Intake Velocity", getVelocity());
+
+  }
+  public void intake() {
+    m_intake.setVoltage(5);
+  }
+  public void reverse_intake () {
+    m_intake.setVoltage(-5);
+
+  }
+  public void stop() {
+    m_intake.stopMotor();
+  }
+  public Command test_forwardsCommand() {
+    return Commands.runEnd(
+        ()-> this.intake(),
+        this::stop,
+        this);
+  }
+    public Command test_reverseCommand() {
+        return Commands.runEnd(
+        ()-> this.intake(),
+        this::stop,
+        this);
+  }
+}
