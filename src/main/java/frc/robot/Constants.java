dcce059c91054554ab2e46064e1bce5d476960b6
CallMeHeyy <113743812+CallMeHeyy@users.noreply.github.com>
2024-03-26 21:30:42 -0500

    checkin

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -176,7 +176,7 @@ public static class IntakePivotConstants{
 	public static final int reverseSoftLimit = 0;
 	public static final int forwardSoftLimit = 0;
 	public static final boolean encoderDirection = false;
-	public static final double encoderOffset = -133.5-119.44;
+	public static final double encoderOffset = -133.5-119.44+60;
 
 	public static final double kP = 0;
 	public static final double kI = 0;
